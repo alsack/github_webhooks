37a69a86607f071f096c9b21e74a4d86515057ed
Andrew Sack <sacko87@gmail.com>
2019-02-23 08:31:28 -0500

    minor updates

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const router = require('./router');
 
 // launch server
-const nodeWebServer = require('node_web_server/src/index.js');
+const nodeWebServer = require('node_web_server');
 
 // add routes
 nodeWebServer.app.use('/', router);
